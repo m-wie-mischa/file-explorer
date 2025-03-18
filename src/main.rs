623045796cb84f9445c39f85ccc8c9ee237bda5e
m-wie-mischa <mischa.google@gmx.de>
2025-03-18 11:54:18 +0100

    rfd module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use rfd::FileDialog;
+
 fn main() {
-    println!("Hello, world!");
+    let files = FileDialog::new()
+        .set_title("Me like Cookie!")
+        .add_filter("text", &["txt", "rs"])
+        .add_filter("rust", &["rs", "toml"])
+        .set_directory("/")
+        .pick_file();
+    println!("{:?}", files)
 }
