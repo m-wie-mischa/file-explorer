e072a7da6ba2300a81dab1b3db533f3c91fbdad6
m-wie-mischa <mischa.google@gmx.de>
2025-03-18 20:46:45 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use rfd::FileDialog;
 
 fn main() {
     let files = FileDialog::new()
-        .set_title("Me like Cookie!")
-        .add_filter("text", &["txt", "rs"])
-        .add_filter("rust", &["rs", "toml"])
+        .set_title("Select Image")
+        .add_filter("image", &["png", "jpg", "jpeg"])
         .set_directory("/")
         .pick_file();
     println!("{:?}", files)
